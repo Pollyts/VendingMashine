db59feb193d75b8c9885426c24cf15c26dcf2f86
tspo <polia.tselisheva@mail.ru>
2021-08-02 16:53:43 +0500

    Added counter in UserPage

--- VendingMashine/ClientApp/src/components/AdminView.jsx
+++ VendingMashine/ClientApp/src/components/AdminView.jsx
@@ -1,6 +1,13 @@
 ﻿import React, { Component } from 'react';
 
 export class AdminView extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { search: "", folders: null, elements: null, breadCrumbs: null };
+        this.handleClick = this.handleClick.bind(this);
+        this.Search = this.Search.bind(this);
+    }
     render() {
         const prodId = this.props.match.params.key;
         return (
--- VendingMashine/ClientApp/src/components/UserView.jsx
+++ VendingMashine/ClientApp/src/components/UserView.jsx
@@ -1,10 +1,31 @@
 ﻿import React, { Component } from 'react';
 
 export class UserView extends Component {
+
+    constructor(props) {
+        super(props);
+        this.AddCoin = this.AddCoin.bind(this);
+        this.state = { count:0 };
+    }
+    AddCoin = coin => e => {
+        this.setState({ count: this.state.count + coin })
+    };
     render() {
-        return (
+        return (            
             <div>
-                HelloWorld
+                {this.state.count}
+                <button onClick={this.AddCoin(1)}>
+                    1
+        </button>
+                <button onClick={this.AddCoin(2)}>
+                    2
+        </button>
+                <button onClick={this.AddCoin(5)}>
+                    5
+        </button>
+                <button onClick={this.AddCoin(10)}>
+                    10
+        </button>
             </div>
         );
     }
